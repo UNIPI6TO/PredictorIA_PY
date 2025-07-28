770817464111816afc413db3b12299b5b781be50
WilsonSalinasFlores <63874104+WilsonSalinasFlores@users.noreply.github.com>
2025-07-27 21:01:55 -0500

    refactoriza y actualizar el gitignore

--- main.py
+++ main.py
@@ -1,30 +1,23 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 import pickle
 import pandas as pd
+
+from personalidadModel import PerfilPersonalidad
 
 # Inicializar la aplicación FastAPI
 app = FastAPI()
 
-# Modelo de entrada para la API
-class PerfilPersonalidad(BaseModel):
-    Extroversión: float
-    Responsabilidad: float
-    Amabilidad: float
-    Neuroticismo: float
-    Apertura: float
 
 # Cargar el modelo entrenado
 def cargar_modelo(ruta: str):
     with open(ruta, 'rb') as file:
         return pickle.load(file)
 
-modelo = cargar_modelo('modelo_personalidad.pkl')
 
 # Endpoint informativo
 @app.get("/info")
 def obtener_info():
-    return {"mensaje": "API para predicción de personalidad"}
+    return {"mensaje": "API para predicción de test"}
 
 # Endpoint de predicción
 @app.post("/predecir_personalidad_rasgos/")
@@ -32,6 +25,8 @@ def predecir_personalidad(perfil: PerfilPersonalidad):
     """
     Realiza una predicción de perfil de personalidad.
     """
+    modelo = cargar_modelo('modelo_personalidad.pkl')
+
     datos_df = pd.DataFrame([perfil.dict()])
     prediccion = modelo.predict(datos_df)
     return {"perfil": prediccion[0]}
